1e00f240521d01723acca5a00056b84b73120a91
Arunanshu Biswas <mydellpc07@gmail.com>
2024-03-12 12:17:01 +0530

    feat(components): add slider

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="flex flex-col px-2 pt-4 gap-4">
-    <div class="flex w-full max-w-sm items-center gap-1.5 mx-auto">
-      <Input id="text" placeholder="How do you feel" v-model="sentiment" />
-      <Button type="submit" @click="execute()">Search</Button>
+  <div class="flex flex-col gap-4 px-2 pt-4">
+    <div class="mx-auto flex w-full max-w-md flex-col gap-4">
+      <div class="flex items-center gap-1.5">
+        <Input id="text" placeholder="How do you feel" v-model="sentiment" />
+        <Button type="submit" @click="execute()">Search</Button>
+      </div>
+      <div class="flex gap-4">
+        <Slider v-model="popularity" />
+        <span>
+          {{ popularityValue }}
+        </span>
+      </div>
     </div>
 
     <div class="grid grid-cols-3 gap-4" v-if="data && !pending">
@@ -44,13 +52,15 @@
 <script setup lang="ts">
 const sentiment = ref('')
 const usePopularity = ref(true)
-const popularity = ref(20)
+const popularity = ref([20])
 const limit = ref(5)
+
+const popularityValue = computed(() => popularity.value[0])
 
 const { data, execute, pending, status } = await useSentiment(
   sentiment,
   usePopularity,
-  popularity,
-  limit
+  limit,
+  popularityValue
 )
 </script>
